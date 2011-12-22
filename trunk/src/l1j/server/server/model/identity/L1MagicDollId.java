79392cb76963d48a5969334551466d446bcf8214
QQ736897217@gmail.com <QQ736897217@gmail.com@5da39b3c-4b31-0f6e-b2bd-1986a9058c0f>
2011-12-22 14:36:29 +0000

    新增 MagicDoll-h.tbl 字串资料

---
+++ trunk/src/l1j/server/server/model/identity/L1MagicDollId.java
@@ -0,0 +1,66 @@
+package l1j.server.server.model.identity;
+
+/**
+ * <h1> MagicDoll-h 字串编号 (29)
+ */
+public class L1MagicDollId {
+	/** 魔法娃娃：小思克巴 */
+	public static final int $0 = 5919;
+	/** 魔法娃娃：公主 */
+	public static final int $1 = 5933;
+	/** 魔法娃娃：野狼宝宝 */
+	public static final int $2 = 6096;
+	/** 魔法娃娃：肥肥 */
+	public static final int $3 = 6100;
+	/** 魔法娃娃：长老 */
+	public static final int $4 = 6443;
+	/** 魔法娃娃：奎斯坦修 */
+	public static final int $5 = 6449;
+	/** 魔法娃娃：石头高仑 */
+	public static final int $6 = 6452;
+	/** 魔法娃娃：王子(♂) */
+	public static final int $7 = 6457;
+	/** 魔法娃娃：公主(♀) */
+	public static final int $8 = 6459;
+	/** 魔法娃娃：骑士(♂) */
+	public static final int $9 = 6461;
+	/** 魔法娃娃：骑士(♀) */
+	public static final int $10 = 6463;
+	/** 魔法娃娃：法师(♂) */
+	public static final int $11 = 6465;
+	/** 魔法娃娃：法师(♀) */
+	public static final int $12 = 6467;
+	/** 魔法娃娃：精灵(♂) */
+	public static final int $13 = 6469;
+	/** 魔法娃娃：精灵(♀) */
+	public static final int $14 = 6471;
+	/** 魔法娃娃：黑暗精灵(♂) */
+	public static final int $15 = 6473;
+	/** 魔法娃娃：黑暗精灵(♀) */
+	public static final int $16 = 6475;
+	/** 魔法娃娃：希尔黛斯 */
+	public static final int $17 = 6477;
+	/** 魔法娃娃：雪怪 */
+	public static final int $18 = 6480;
+	/** 魔法娃娃：蛇女 */
+	public static final int $19 = 6483;
+	/** 魔法娃娃：亚力安 */
+	public static final int $20 = 7047;
+	/** 魔法娃娃：稻草人 */
+	public static final int $21 = 7050;
+	/** 魔法娃娃：史巴托 */
+	public static final int $22 = 7257;
+	/** 魔法娃娃：(不知叫啥名..) */
+	public static final int $23 = 7516;
+	/** 神秘棱镜：高等幼龙(♂) */
+	public static final int $24 = 7659;
+	/** 神秘棱镜：高等幼龙(♀) */
+	public static final int $25 = 7660;
+	/** 神秘棱镜：幼龙(♂) */
+	public static final int $26 = 7661;
+	/** 神秘棱镜：幼龙(♀) */
+	public static final int $27 = 7662;
+	/** 魔法娃娃：雪人(A.B.C.D.商城) */
+	public static final int $28 = 8751;
+
+}
