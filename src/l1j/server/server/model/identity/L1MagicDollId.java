ecb0bb52ded8425da80115802580d67aee69fd63
QQ736897217 <QQ736897217@gmail.com>
2011-12-28 12:40:51 +0000

    更新 magicdoll.tbl 资料

--- src/l1j/server/server/model/identity/L1MagicDollId.java
+++ src/l1j/server/server/model/identity/L1MagicDollId.java
@@ -1,66 +1,114 @@
 package l1j.server.server.model.identity;
 
 /**
- * <h1> MagicDoll-h 字串编号 (29)
+ * <h1> magicdoll.tbl 字串编号 (52)
  */
 public class L1MagicDollId {
 	/** 魔法娃娃：小思克巴 */
-	public static final int $0 = 5919;
+	public static final int $5919 = 0;
 	/** 魔法娃娃：公主 */
-	public static final int $1 = 5933;
+	public static final int $5933 = 1;
 	/** 魔法娃娃：野狼宝宝 */
-	public static final int $2 = 6096;
+	public static final int $6096 = 2;
 	/** 魔法娃娃：肥肥 */
-	public static final int $3 = 6100;
+	public static final int $6100 = 3;
 	/** 魔法娃娃：长老 */
-	public static final int $4 = 6443;
+	public static final int $6443 = 4;
 	/** 魔法娃娃：奎斯坦修 */
-	public static final int $5 = 6449;
+	public static final int $6449 = 5;
 	/** 魔法娃娃：石头高仑 */
-	public static final int $6 = 6452;
+	public static final int $6452 = 6;
 	/** 魔法娃娃：王子(♂) */
-	public static final int $7 = 6457;
+	public static final int $6457 = 7;
 	/** 魔法娃娃：公主(♀) */
-	public static final int $8 = 6459;
+	public static final int $6459 = 8;
 	/** 魔法娃娃：骑士(♂) */
-	public static final int $9 = 6461;
+	public static final int $6461 = 9;
 	/** 魔法娃娃：骑士(♀) */
-	public static final int $10 = 6463;
+	public static final int $6463 = 10;
 	/** 魔法娃娃：法师(♂) */
-	public static final int $11 = 6465;
+	public static final int $6465 = 11;
 	/** 魔法娃娃：法师(♀) */
-	public static final int $12 = 6467;
+	public static final int $6467 = 12;
 	/** 魔法娃娃：精灵(♂) */
-	public static final int $13 = 6469;
+	public static final int $6469 = 13;
 	/** 魔法娃娃：精灵(♀) */
-	public static final int $14 = 6471;
+	public static final int $6471 = 14;
 	/** 魔法娃娃：黑暗精灵(♂) */
-	public static final int $15 = 6473;
+	public static final int $6473 = 15;
 	/** 魔法娃娃：黑暗精灵(♀) */
-	public static final int $16 = 6475;
+	public static final int $6475 = 16;
 	/** 魔法娃娃：希尔黛斯 */
-	public static final int $17 = 6477;
+	public static final int $6477 = 17;
 	/** 魔法娃娃：雪怪 */
-	public static final int $18 = 6480;
+	public static final int $6480 = 18;
 	/** 魔法娃娃：蛇女 */
-	public static final int $19 = 6483;
+	public static final int $6483 = 19;
 	/** 魔法娃娃：亚力安 */
-	public static final int $20 = 7047;
+	public static final int $7047 = 20;
 	/** 魔法娃娃：稻草人 */
-	public static final int $21 = 7050;
+	public static final int $7050 = 21;
 	/** 魔法娃娃：史巴托 */
-	public static final int $22 = 7257;
+	public static final int $7257 = 22;
 	/** 魔法娃娃：(不知叫啥名..) */
-	public static final int $23 = 7516;
+	public static final int $7516 = 23;
 	/** 神秘棱镜：高等幼龙(♂) */
-	public static final int $24 = 7659;
+	public static final int $7659 = 24;
 	/** 神秘棱镜：高等幼龙(♀) */
-	public static final int $25 = 7660;
+	public static final int $7660 = 25;
 	/** 神秘棱镜：幼龙(♂) */
-	public static final int $26 = 7661;
+	public static final int $7661 = 26;
 	/** 神秘棱镜：幼龙(♀) */
-	public static final int $27 = 7662;
+	public static final int $7662 = 27;
 	/** 魔法娃娃：雪人(A.B.C.D.商城) */
-	public static final int $28 = 8751;
+	public static final int $8741 = 28;
+	/** 魔法娃娃：雪人 */
+	public static final int $8751 = 29;
+	/** 魔法娃娃：巨人 */
+	public static final int $8539 = 30;
+	/** 魔法娃娃：巨人 */
+	public static final int $8542 = 31;
+	/**  */
+	public static final int $8543 = 32;
+	/**  */
+	public static final int $8544 = 33;
+	/**  */
+	public static final int $8545 = 34;
+	/**  */
+	public static final int $8546 = 35;
+	/**  */
+	public static final int $8549 = 36;
+	/**  */
+	public static final int $8600 = 37;
+	/**  */
+	public static final int $8601 = 38;
+	/**  */
+	public static final int $8602 = 39;
+	/**  */
+	public static final int $8603 = 40;
+	/**  */
+	public static final int $8650 = 41;
+	/**  */
+	public static final int $8653 = 42;
+	/**  */
+	public static final int $8656 = 43;
+	/**  */
+	public static final int $8694 = 44;
+	/**  */
+	public static final int $8695 = 45;
+	/**  */
+	public static final int $8696 = 46;
+	/**  */
+	public static final int $8697 = 47;
+	/**  */
+	public static final int $8743 = 48;
+	/**  */
+	public static final int $8744 = 49;
+	/**  */
+	public static final int $8758 = 50;
+	/**  */
+	public static final int $8749 = 51;
+	/**  */
+	public static final int $8750 = 52;
 
 }
