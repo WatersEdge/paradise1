1a044faf4179a6fac0435330cb01da4e7643fa6a
QQ736897217 <QQ736897217@gmail.com>
2012-01-17 04:42:29 +0000

    调整

--- src/lineage/item/etcitem/potion/mp/BluePotion.java
+++ src/lineage/item/etcitem/potion/mp/BluePotion.java
@@ -1,15 +1,13 @@
-package l1j.server.data.item_etcitem.mp;
+package lineage.item.etcitem.potion.mp;
 
 import static l1j.server.server.model.skill.L1SkillId.STATUS_BLUE_POTION;
-
-import l1j.server.data.executor.ItemExecutor;
-import l1j.server.data.executor.remove.RemoveSkillEffect;
-import l1j.server.data.executor.stop.StopUsePotion;
 import l1j.server.server.model.Instance.L1ItemInstance;
 import l1j.server.server.model.Instance.L1PcInstance;
 import l1j.server.server.serverpackets.S_ServerMessage;
 import l1j.server.server.serverpackets.S_SkillIconGFX;
 import l1j.server.server.serverpackets.S_SkillSound;
+import lineage.console.executor.ItemExecutor;
+import lineage.console.remove.RemoveSkillEffect;
 
 /**
  * 蓝色药水 40015<br>
@@ -34,44 +32,38 @@ public class BluePotion extends ItemExecutor {
 	 * @param data
 	 *            参数
 	 * @param pc
-	 *            角色
+	 *            对象
 	 * @param item
 	 *            道具
 	 */
 	@Override
-	public void execute(final int[] data, final L1PcInstance pc,
-			final L1ItemInstance item) {
+	public void execute(final int[] data, final L1PcInstance pc, final L1ItemInstance item) {
 
-		// 道具为空
-		if (item == null) {
-			return;
-		}
+		// 取回道具ID
+		final int itemId = item.getItemId();
 
-		// 角色为空
-		if (pc == null) {
-			return;
-		}
+		// 执行效果
+		this.useBluePotion(pc, itemId);
 
-		// 可以使用
-		if (StopUsePotion.stopUsePotion(pc)) {
-			final int itemId = item.getItemId();
-			this.useBluePotion(pc, itemId);
-			pc.getInventory().removeItem(item, 1);
-		}
+		// 删除道具
+		pc.getInventory().removeItem(item, 1);
 	}
 
 	/**
 	 * 执行效果
 	 * 
 	 * @param pc
-	 *            角色
+	 *            对象
 	 * @param item_id
 	 *            道具ID
 	 */
 	private void useBluePotion(final L1PcInstance pc, final int item_id) {
 
-		// 解除绝对屏障效果
-		RemoveSkillEffect.RemoveAbsoluteBarrierEffect(pc);
+		// 删除绝对屏障效果
+		RemoveSkillEffect.removeAbsoluteBarrierEffect(pc);
+
+		// 删除重复的蓝水效果
+		RemoveSkillEffect.removeStatusBluePotion(pc);
 
 		// 给予效果的时间
 		int time = 0;
@@ -91,15 +83,10 @@ public class BluePotion extends ItemExecutor {
 			break;
 		}
 
-		// 删除重复的蓝水状态
-		if (pc.hasSkillEffect(STATUS_BLUE_POTION)) {
-			pc.killSkillEffectTimer(STATUS_BLUE_POTION);
-		}
-
 		pc.sendPackets(new S_SkillIconGFX(34, time)); // 发送状态图示
 		pc.setSkillEffect(STATUS_BLUE_POTION, time * 1000); // 给予蓝水时间 (秒)
-		pc.sendPackets(new S_SkillSound(pc.getId(), 190));
-		pc.broadcastPacket(new S_SkillSound(pc.getId(), 190));
+		pc.sendPackets(new S_SkillSound(pc.getId(), 190)); // 效果动画 (自己看得到)
+		pc.broadcastPacket(new S_SkillSound(pc.getId(), 190)); // 效果动画 (同画面的其他人看得到)
 		pc.sendPackets(new S_ServerMessage(1007)); // 你感觉到魔力恢复速度加快。
 	}
 }
