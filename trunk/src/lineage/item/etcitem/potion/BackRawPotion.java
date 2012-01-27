d56f86948e43d64e39ca10946605300131d17369
QQ736897217@gmail.com <QQ736897217@gmail.com@5da39b3c-4b31-0f6e-b2bd-1986a9058c0f>
2012-01-27 06:18:00 +0000

    补上 返生药水与万能药 (力敏体智精魅)

---
+++ trunk/src/lineage/item/etcitem/potion/BackRawPotion.java
@@ -0,0 +1,65 @@
+package lineage.item.etcitem.potion;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import l1j.server.server.model.Instance.L1ItemInstance;
+import l1j.server.server.model.Instance.L1PcInstance;
+import l1j.server.server.serverpackets.S_OwnCharStatus;
+import l1j.server.server.serverpackets.S_ServerMessage;
+import l1j.server.server.serverpackets.S_SkillSound;
+import lineage.console.executor.ItemExecutor;
+
+/**
+ * 返生药水 - 43000
+ * 
+ * @author jrwz
+ */
+public class BackRawPotion extends ItemExecutor {
+
+	private static Logger _log = Logger.getLogger(BackRawPotion.class.getName());
+
+	private BackRawPotion() {
+	}
+
+	public static ItemExecutor get() {
+		return new BackRawPotion();
+	}
+
+	/**
+	 * 道具执行
+	 * 
+	 * @param data
+	 *            参数
+	 * @param pc
+	 *            对象
+	 * @param item
+	 *            道具
+	 */
+	@Override
+	public void execute(final int[] data, final L1PcInstance pc, final L1ItemInstance item) {
+
+		try {
+			pc.setExp(1); // 总经验值归1
+			pc.resetLevel(); // 等级重算
+			pc.setBonusStats(0); // 清空奖励属性点记录 (升级可以继续点奖励属性点)
+			pc.getInventory().takeoffEquip(945); // 用来脱掉全身装备
+			pc.sendPackets(new S_OwnCharStatus(pc)); // 更新角色属性与能力值
+			pc.sendPackets(new S_ServerMessage(822)); // 你感受到体内深处产生一股不明力量。
+			pc.sendPackets(new S_SkillSound(pc.getId(), 2568)); // 风龙降临动画特效。(自己)
+			pc.broadcastPacket(new S_SkillSound(pc.getId(), 2568)); // 风龙降临动画特效。(可见范围的其他角色)
+			pc.save(); // 将玩家资料储存到资料库中
+		}
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			_log.log(Level.SEVERE, e.getLocalizedMessage(), e);
+			throw new Error("返生药水错误" + " File.");
+		}
+
+		// 处理新手保护系统(遭遇的守护)状态资料的变动
+		pc.checkNoviceType();
+
+		// 删除道具
+		pc.getInventory().removeItem(item, 1);
+	}
+}
---
+++ trunk/src/lineage/item/etcitem/potion/Nostrum_CON.java
@@ -0,0 +1,61 @@
+package lineage.item.etcitem.potion;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import l1j.server.server.model.Instance.L1ItemInstance;
+import l1j.server.server.model.Instance.L1PcInstance;
+import l1j.server.server.serverpackets.S_OwnCharStatus2;
+import l1j.server.server.serverpackets.S_ServerMessage;
+import lineage.console.executor.ItemExecutor;
+
+/**
+ * 万能药:体质 - 40034
+ * 
+ * @author jrwz
+ */
+public class Nostrum_CON extends ItemExecutor {
+
+	private static Logger _log = Logger.getLogger(Nostrum_CON.class.getName());
+
+	private Nostrum_CON() {
+	}
+
+	public static ItemExecutor get() {
+		return new Nostrum_CON();
+	}
+
+	/**
+	 * 道具执行
+	 * 
+	 * @param data
+	 *            参数
+	 * @param pc
+	 *            对象
+	 * @param item
+	 *            道具
+	 */
+	@Override
+	public void execute(final int[] data, final L1PcInstance pc, final L1ItemInstance item) {
+
+		try {
+			if ((pc.getBaseCon() < 35) && (pc.getElixirStats() < 5)) {
+				pc.addBaseCon((byte) 1); // CON+1
+				pc.setElixirStats(pc.getElixirStats() + 1); // 万能药使用记录+1
+				pc.sendPackets(new S_OwnCharStatus2(pc)); // 更新六项能力值以及负重
+				pc.save(); // 将玩家资料储存到资料库中
+			}
+			else {
+				pc.sendPackets(new S_ServerMessage(481)); // \f1属性最大值只能到35。 请重试一次。
+			}
+		}
+		catch (Exception e) {
+			// TODO Auto-generated catch block
+			_log.log(Level.SEVERE, e.getLocalizedMessage(), e);
+			throw new Error("万能药:体质 错误" + " File.");
+		}
+
+		// 删除道具
+		pc.getInventory().removeItem(item, 1);
+	}
+}
