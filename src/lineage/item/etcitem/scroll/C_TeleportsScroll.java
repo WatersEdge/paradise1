423ddd1f798007718501eca0ace48ced59b96009
QQ736897217 <QQ736897217@gmail.com>
2012-02-09 01:59:33 +0000

    补上 瞬间移动卷轴

---
+++ src/lineage/item/etcitem/scroll/C_TeleportsScroll.java
@@ -0,0 +1,38 @@
+package lineage.item.etcitem.scroll;
+
+import l1j.server.server.model.L1Teleport;
+import l1j.server.server.model.Instance.L1ItemInstance;
+import l1j.server.server.model.Instance.L1PcInstance;
+import lineage.console.executor.ItemExecutor;
+
+/**
+ * 受诅咒的 瞬间移动卷轴 - 240100 <br>
+ * 
+ * @author jrwz
+ */
+public class C_TeleportsScroll extends ItemExecutor {
+
+	private C_TeleportsScroll() {
+	}
+
+	public static ItemExecutor get() {
+		return new C_TeleportsScroll();
+	}
+
+	/**
+	 * 道具执行
+	 * 
+	 * @param data
+	 *            参数
+	 * @param pc
+	 *            对象
+	 * @param item
+	 *            道具
+	 */
+	@Override
+	public void execute(final int[] data, final L1PcInstance pc, final L1ItemInstance item) {
+
+		L1Teleport.teleport(pc, pc.getX(), pc.getY(), pc.getMapId(), pc.getHeading(), true);
+		pc.getInventory().removeItem(item, 1);
+	}
+}
