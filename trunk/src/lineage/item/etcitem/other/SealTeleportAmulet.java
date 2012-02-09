34ffd857d5339fcd57a423d5531745688a99159a
QQ736897217@gmail.com <QQ736897217@gmail.com@5da39b3c-4b31-0f6e-b2bd-1986a9058c0f>
2012-02-09 08:06:28 +0000

    补上 傲慢之塔传送符

---
+++ trunk/src/lineage/item/etcitem/other/SealTeleportAmulet.java
@@ -0,0 +1,42 @@
+package lineage.item.etcitem.other;
+
+import l1j.server.server.model.Instance.L1ItemInstance;
+import l1j.server.server.model.Instance.L1PcInstance;
+import l1j.server.server.serverpackets.S_ServerMessage;
+import lineage.console.executor.ItemExecutor;
+
+/**
+ * 封印的傲慢之塔传送符 (11F-91F) (40280 - 40288)
+ * 
+ * @author jrwz
+ */
+public class SealTeleportAmulet extends ItemExecutor {
+
+	private SealTeleportAmulet() {
+	}
+
+	public static ItemExecutor get() {
+		return new SealTeleportAmulet();
+	}
+
+	/**
+	 * 道具执行
+	 * 
+	 * @param data
+	 *            参数
+	 * @param pc
+	 *            对象
+	 * @param item
+	 *            道具
+	 */
+	@Override
+	public void execute(final int[] data, final L1PcInstance pc, final L1ItemInstance item) {
+
+		pc.getInventory().removeItem(item, 1);
+		final int itemId = item.getItemId();
+		L1ItemInstance item1 = pc.getInventory().storeItem(itemId + 9, 1);
+		if (item != null) {
+			pc.sendPackets(new S_ServerMessage(403, item1.getLogName())); // 获得%0%o 。
+		}
+	}
+}
