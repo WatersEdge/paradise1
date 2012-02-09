ad60eb498894e20588d3011bad1e8fd3013423f6
QQ736897217@gmail.com <QQ736897217@gmail.com@5da39b3c-4b31-0f6e-b2bd-1986a9058c0f>
2012-02-09 13:13:04 +0000

    补上 一些任务道具与传送道具

---
+++ trunk/src/lineage/item/etcitem/teleport/WeakSoul.java
@@ -0,0 +1,45 @@
+package lineage.item.etcitem.teleport;
+
+import l1j.server.server.model.L1Teleport;
+import l1j.server.server.model.Instance.L1ItemInstance;
+import l1j.server.server.model.Instance.L1PcInstance;
+import l1j.server.server.serverpackets.S_ServerMessage;
+import l1j.server.server.templates.L1EtcItem;
+import lineage.console.executor.ItemExecutor;
+
+/**
+ * 微弱的灵魂 - 41208
+ * 
+ * @author jrwz
+ */
+public class WeakSoul extends ItemExecutor {
+
+	private WeakSoul() {
+	}
+
+	public static ItemExecutor get() {
+		return new WeakSoul();
+	}
+
+	/**
+	 * 道具执行
+	 * 
+	 * @param data
+	 *            参数
+	 * @param pc
+	 *            对象
+	 * @param item
+	 *            道具
+	 */
+	@Override
+	public void execute(final int[] data, final L1PcInstance pc, final L1ItemInstance item) {
+
+		// 船舶之墓:地上层
+		if (((pc.getX() >= 32844) && (pc.getX() <= 32845)) && ((pc.getY() >= 32693) && (pc.getY() <= 32694)) && (pc.getMapId() == 550)) {
+			L1Teleport.teleport(pc, ((L1EtcItem) item.getItem()).get_locx(), ((L1EtcItem) item.getItem()).get_locy(), ((L1EtcItem) item.getItem()).get_mapid(), 5, true);
+		}
+		else {
+			pc.sendPackets(new S_ServerMessage(79)); // \f1没有任何事情发生。
+		}
+	}
+}
