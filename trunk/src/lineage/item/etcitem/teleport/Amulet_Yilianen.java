2da57437e2268186ce91f41a128c8e86b695cbae
QQ736897217@gmail.com <QQ736897217@gmail.com@5da39b3c-4b31-0f6e-b2bd-1986a9058c0f>
2012-02-09 03:18:46 +0000

    补上 任务传送道具

---
+++ trunk/src/lineage/item/etcitem/teleport/Amulet_Yilianen.java
@@ -0,0 +1,46 @@
+package lineage.item.etcitem.teleport;
+
+import l1j.server.server.model.L1Quest;
+import l1j.server.server.model.L1Teleport;
+import l1j.server.server.model.Instance.L1ItemInstance;
+import l1j.server.server.model.Instance.L1PcInstance;
+import l1j.server.server.serverpackets.S_ServerMessage;
+import lineage.console.executor.ItemExecutor;
+
+/**
+ * 希莲恩的护身符 - 49178
+ * 
+ * @author jrwz
+ */
+public class Amulet_Yilianen extends ItemExecutor {
+
+	private Amulet_Yilianen() {
+	}
+
+	public static ItemExecutor get() {
+		return new Amulet_Yilianen();
+	}
+
+	/**
+	 * 道具执行
+	 * 
+	 * @param data
+	 *            参数
+	 * @param pc
+	 *            对象
+	 * @param item
+	 *            道具
+	 */
+	@Override
+	public void execute(final int[] data, final L1PcInstance pc, final L1ItemInstance item) {
+
+		if ((pc.isIllusionist()) && (pc.getMapId() == 2004) && (pc.getQuest().get_step(L1Quest.QUEST_LEVEL50) > 1)) {
+			final short mapid = 1000;
+			L1Teleport.teleport(pc, 32772, 32812, mapid, 5, true);
+			pc.getInventory().removeItem(item, 1);
+		}
+		else {
+			pc.sendPackets(new S_ServerMessage(79)); // \f1没有任何事情发生。
+		}
+	}
+}
---
+++ trunk/src/lineage/item/etcitem/teleport/PrivateRoomKey.java
@@ -0,0 +1,44 @@
+package lineage.item.etcitem.teleport;
+
+import l1j.server.server.model.L1Teleport;
+import l1j.server.server.model.Instance.L1ItemInstance;
+import l1j.server.server.model.Instance.L1PcInstance;
+import l1j.server.server.serverpackets.S_ServerMessage;
+import lineage.console.executor.ItemExecutor;
+
+/**
+ * 密室钥匙 - 40555
+ * 
+ * @author jrwz
+ */
+public class PrivateRoomKey extends ItemExecutor {
+
+	private PrivateRoomKey() {
+	}
+
+	public static ItemExecutor get() {
+		return new PrivateRoomKey();
+	}
+
+	/**
+	 * 道具执行
+	 * 
+	 * @param data
+	 *            参数
+	 * @param pc
+	 *            对象
+	 * @param item
+	 *            道具
+	 */
+	@Override
+	public void execute(final int[] data, final L1PcInstance pc, final L1ItemInstance item) {
+
+		if (pc.isKnight() && ((pc.getX() >= 32806) && (pc.getX() <= 32814)) && ((pc.getY() >= 32798) && (pc.getY() <= 32807)) && (pc.getMapId() == 13)) {
+			final short mapid = 13;
+			L1Teleport.teleport(pc, 32815, 32810, mapid, 5, false);
+		}
+		else {
+			pc.sendPackets(new S_ServerMessage(79)); // \f1没有任何事情发生。
+		}
+	}
+}
