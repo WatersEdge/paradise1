d54f74b1d29e2e8931d770b8a193ae7901f9fc06
QQ736897217@gmail.com <QQ736897217@gmail.com@5da39b3c-4b31-0f6e-b2bd-1986a9058c0f>
2012-02-10 03:07:46 +0000

    补上 一些任务合成道具 (龟裂之核、宝箱碎片等)

---
+++ trunk/src/lineage/item/etcitem/quest/EvilDebris_2.java
@@ -0,0 +1,47 @@
+package lineage.item.etcitem.quest;
+
+import l1j.server.server.model.Instance.L1ItemInstance;
+import l1j.server.server.model.Instance.L1PcInstance;
+import l1j.server.server.serverpackets.S_ServerMessage;
+import lineage.console.executor.ItemExecutor;
+
+/**
+ * 第二次邪念碎片 - 49198
+ * 
+ * @author jrwz
+ */
+public class EvilDebris_2 extends ItemExecutor {
+
+	private EvilDebris_2() {
+	}
+
+	public static ItemExecutor get() {
+		return new EvilDebris_2();
+	}
+
+	/**
+	 * 道具执行
+	 * 
+	 * @param data
+	 *            参数
+	 * @param pc
+	 *            对象
+	 * @param item
+	 *            道具
+	 */
+	@Override
+	public void execute(final int[] data, final L1PcInstance pc, final L1ItemInstance item) {
+
+		final int itemobj = data[0];
+		final L1ItemInstance l1iteminstance1 = pc.getInventory().getItem(itemobj);
+
+		if (l1iteminstance1.getItem().getItemId() == 49197) {
+			pc.getInventory().consumeItem(49197, 1);
+			pc.getInventory().consumeItem(49198, 1);
+			pc.createNewItem(pc, 49200, 1);
+		}
+		else {
+			pc.sendPackets(new S_ServerMessage(79)); // \f1没有任何事情发生。
+		}
+	}
+}
---
+++ trunk/src/lineage/item/etcitem/quest/TreasureBoxDebris_Osiris_2.java
@@ -0,0 +1,47 @@
+package lineage.item.etcitem.quest;
+
+import l1j.server.server.model.Instance.L1ItemInstance;
+import l1j.server.server.model.Instance.L1PcInstance;
+import l1j.server.server.serverpackets.S_ServerMessage;
+import lineage.console.executor.ItemExecutor;
+
+/**
+ * 欧西里斯高级宝箱碎片(下) - 49098
+ * 
+ * @author jrwz
+ */
+public class TreasureBoxDebris_Osiris_2 extends ItemExecutor {
+
+	private TreasureBoxDebris_Osiris_2() {
+	}
+
+	public static ItemExecutor get() {
+		return new TreasureBoxDebris_Osiris_2();
+	}
+
+	/**
+	 * 道具执行
+	 * 
+	 * @param data
+	 *            参数
+	 * @param pc
+	 *            对象
+	 * @param item
+	 *            道具
+	 */
+	@Override
+	public void execute(final int[] data, final L1PcInstance pc, final L1ItemInstance item) {
+
+		final int itemobj = data[0];
+		final L1ItemInstance l1iteminstance1 = pc.getInventory().getItem(itemobj);
+
+		if (l1iteminstance1.getItem().getItemId() == 49097) {
+			pc.getInventory().consumeItem(49097, 1);
+			pc.getInventory().consumeItem(49098, 1);
+			pc.createNewItem(pc, 49099, 1);
+		}
+		else {
+			pc.sendPackets(new S_ServerMessage(79)); // \f1没有任何事情发生。
+		}
+	}
+}
