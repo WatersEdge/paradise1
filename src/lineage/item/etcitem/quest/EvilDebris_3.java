b6eeb5990c6315467eb310f28976dfdf31d80cef
QQ736897217 <QQ736897217@gmail.com>
2012-02-10 03:07:46 +0000

    补上 一些任务合成道具 (龟裂之核、宝箱碎片等)

---
+++ src/lineage/item/etcitem/quest/EvilDebris_3.java
@@ -0,0 +1,47 @@
+package lineage.item.etcitem.quest;
+
+import l1j.server.server.model.Instance.L1ItemInstance;
+import l1j.server.server.model.Instance.L1PcInstance;
+import l1j.server.server.serverpackets.S_ServerMessage;
+import lineage.console.executor.ItemExecutor;
+
+/**
+ * 第三次邪念碎片 - 49199
+ * 
+ * @author jrwz
+ */
+public class EvilDebris_3 extends ItemExecutor {
+
+	private EvilDebris_3() {
+	}
+
+	public static ItemExecutor get() {
+		return new EvilDebris_3();
+	}
+
+	/**
+	 * 道具执行
+	 * 
+	 * @param data
+	 *            参数
+	 * @param pc
+	 *            对象
+	 * @param item
+	 *            道具
+	 */
+	@Override
+	public void execute(final int[] data, final L1PcInstance pc, final L1ItemInstance item) {
+
+		final int itemobj = data[0];
+		final L1ItemInstance l1iteminstance1 = pc.getInventory().getItem(itemobj);
+
+		if (l1iteminstance1.getItem().getItemId() == 49200) {
+			pc.getInventory().consumeItem(49199, 1);
+			pc.getInventory().consumeItem(49200, 1);
+			pc.createNewItem(pc, 49201, 1);
+		}
+		else {
+			pc.sendPackets(new S_ServerMessage(79)); // \f1没有任何事情发生。
+		}
+	}
+}
