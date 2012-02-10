d4ddf41ea5d604085390ec585bfbd6c5719aff61
QQ736897217 <QQ736897217@gmail.com>
2012-02-10 04:02:56 +0000

    补上 太古的玉玺与毁灭盔甲的卷轴

---
+++ src/lineage/item/etcitem/SwireYuxi.java
@@ -0,0 +1,68 @@
+package lineage.item.etcitem;
+
+import l1j.server.Config;
+import l1j.server.server.Account;
+import l1j.server.server.model.Instance.L1ItemInstance;
+import l1j.server.server.model.Instance.L1PcInstance;
+import l1j.server.server.serverpackets.S_ServerMessage;
+import lineage.console.executor.ItemExecutor;
+
+/**
+ * 太古的玉玺 - 41428 (可额外新增一个角色)
+ * 
+ * @author jrwz
+ */
+public class SwireYuxi extends ItemExecutor {
+
+	private SwireYuxi() {
+	}
+
+	public static ItemExecutor get() {
+		return new SwireYuxi();
+	}
+
+	/**
+	 * 道具执行
+	 * 
+	 * @param data
+	 *            参数
+	 * @param pc
+	 *            对象
+	 * @param item
+	 *            道具
+	 */
+	@Override
+	public void execute(final int[] data, final L1PcInstance pc, final L1ItemInstance item) {
+
+		if ((pc != null) && (item != null)) {
+			final Account account = Account.load(pc.getAccountName());
+
+			if (account == null) {
+				pc.sendPackets(new S_ServerMessage(79)); // \f1没有任何事情发生。
+				return;
+			}
+
+			int characterSlot = account.getCharacterSlot();
+			final int maxAmount = Config.DEFAULT_CHARACTER_SLOT + characterSlot;
+
+			if (maxAmount >= 8) {
+				pc.sendPackets(new S_ServerMessage(79)); // \f1没有任何事情发生。
+				return;
+			}
+
+			if (characterSlot < 0) {
+				characterSlot = 0;
+			}
+			else {
+				characterSlot += 1;
+			}
+
+			account.setCharacterSlot(characterSlot);
+			Account.updateCharacterSlot(account);
+			pc.getInventory().removeItem(item, 1);
+		}
+		else {
+			pc.sendPackets(new S_ServerMessage(79));
+		}
+	}
+}
