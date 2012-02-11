8d949e5aa1699ce9407cafe786c93b318647e0f2
QQ736897217@gmail.com <QQ736897217@gmail.com@5da39b3c-4b31-0f6e-b2bd-1986a9058c0f>
2012-02-11 17:52:49 +0000

    改写 技能书类道具 接口

--- trunk/src/lineage/item/etcitem/spellbook/CrownSpellBook.java
+++ trunk/src/lineage/item/etcitem/spellbook/CrownSpellBook.java
@@ -2,7 +2,9 @@ package lineage.item.etcitem.spellbook;
 
 import l1j.server.server.model.Instance.L1ItemInstance;
 import l1j.server.server.model.Instance.L1PcInstance;
-import lineage.console.Factory;
+import l1j.server.server.serverpackets.S_ServerMessage;
+import lineage.console.connector.UniversalUseItem;
+import lineage.console.connector.cite.UseSpellBook_Crown;
 import lineage.console.executor.ItemExecutor;
 
 /**
@@ -32,7 +34,14 @@ public class CrownSpellBook extends ItemExecutor {
 	@Override
 	public void execute(final int[] data, final L1PcInstance pc, final L1ItemInstance item) {
 
-		final int itemId = item.getItemId();
-		Factory.getSpellBook().useCrownSpellBook(pc, item, itemId);
+		if (pc.isCrown() || pc.isGm()) {
+			final int itemId = item.getItemId();
+			final UniversalUseItem a = new UseSpellBook_Crown();
+			a.useItem(pc, item, itemId, 0, 0, 0);
+			// Factory.getSpellBook().useCrownSpellBook(pc, item, itemId);
+		}
+		else {
+			pc.sendPackets(new S_ServerMessage(79)); // 没有任何事情发生。
+		}
 	}
 }
