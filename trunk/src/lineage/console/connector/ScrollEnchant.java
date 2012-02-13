fa3c6c2c5243245ad5b11db48e9e961d69c9ae11
QQ736897217@gmail.com <QQ736897217@gmail.com@5da39b3c-4b31-0f6e-b2bd-1986a9058c0f>
2012-02-13 06:28:06 +0000

    改写 强化卷轴

---
+++ trunk/src/lineage/console/connector/ScrollEnchant.java
@@ -0,0 +1,67 @@
+package lineage.console.connector;
+
+import l1j.server.server.model.Instance.L1ItemInstance;
+import l1j.server.server.model.Instance.L1PcInstance;
+
+/**
+ * 强化卷轴接口 (对武器施法的卷轴、对盔甲施法的卷轴、饰品强化卷轴及强化效果)
+ * 
+ * @author jrwz
+ */
+public interface ScrollEnchant {
+
+	/**
+	 * 对武器施法的卷轴
+	 * 
+	 * @param pc
+	 * @param l1iteminstance
+	 * @param l1iteminstance1
+	 */
+	void scrollOfEnchantWeapon(L1PcInstance pc, L1ItemInstance l1iteminstance, L1ItemInstance l1iteminstance1);
+
+	/**
+	 * 对盔甲施法的卷轴
+	 * 
+	 * @param pc
+	 * @param l1iteminstance
+	 * @param l1iteminstance1
+	 */
+	void scrollOfEnchantArmor(L1PcInstance pc, L1ItemInstance l1iteminstance, L1ItemInstance l1iteminstance1);
+
+	/**
+	 * 饰品强化卷轴
+	 * 
+	 * @param pc
+	 * @param l1iteminstance
+	 * @param l1iteminstance1
+	 */
+	void scrollOfEnchantAccessory(L1PcInstance pc, L1ItemInstance l1iteminstance, L1ItemInstance l1iteminstance1);
+
+	/**
+	 * 武器属性强化卷轴
+	 * 
+	 * @param pc
+	 * @param l1iteminstance
+	 * @param l1iteminstance1
+	 */
+	void scrollOfEnchantWeaponAttr(L1PcInstance pc, L1ItemInstance l1iteminstance, L1ItemInstance l1iteminstance1);
+
+	/**
+	 * 象牙塔对武器施法的卷轴
+	 * 
+	 * @param pc
+	 * @param l1iteminstance
+	 * @param l1iteminstance1
+	 */
+	void scrollOfEnchantWeaponIvoryTower(L1PcInstance pc, L1ItemInstance l1iteminstance, L1ItemInstance l1iteminstance1);
+
+	/**
+	 * 象牙塔对盔甲施法的卷轴
+	 * 
+	 * @param pc
+	 * @param l1iteminstance
+	 * @param l1iteminstance1
+	 */
+	void scrollOfEnchantArmorIvoryTower(L1PcInstance pc, L1ItemInstance l1iteminstance, L1ItemInstance l1iteminstance1);
+
+}
